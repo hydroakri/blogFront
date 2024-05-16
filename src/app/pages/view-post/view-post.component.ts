b59612bf32153d45b05a871187b72a0a341fa7e3
hydroakri <hydroakri@protonmail.com>
2024-05-16 20:38:36 +0800

    implement comment

--- src/app/pages/view-post/view-post.component.ts
+++ src/app/pages/view-post/view-post.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { PostService } from '../../service/post.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommentService } from '../../service/comment.service';
 
 @Component({
   selector: 'app-view-post',
@@ -12,15 +14,38 @@ export class ViewPostComponent {
   postId = this.activatedRoute.snapshot.params['id'];
   postData: any;
 
+  commentForm!: FormGroup;
+
   constructor(
     private postService: PostService,
     private activatedRoute: ActivatedRoute,
     private matSnackBar: MatSnackBar,
+    private fb: FormBuilder,
+    private commentService: CommentService,
   ) {}
 
   ngOnInit() {
     console.log(this.postId);
     this.getPostById();
+
+    this.commentForm = this.fb.group({
+      postedBy: [null, Validators.required],
+      content: [null, Validators.required],
+    });
+  }
+
+  publishComment() {
+    const postedBy = this.commentForm.get('postedBy')?.value;
+    const content = this.commentForm.get('content')?.value;
+
+    this.commentService.createComment(this.postId, postedBy, content).subscribe(
+      (res) => {
+        this.matSnackBar.open('Comment Published successfully', 'Ok');
+      },
+      (error) => {
+        this.matSnackBar.open('something went wrong', 'Ok');
+      },
+    );
   }
 
   getPostById() {
@@ -28,6 +53,18 @@ export class ViewPostComponent {
       (res) => {
         this.postData = res;
         console.log(res);
+      },
+      (error) => {
+        this.matSnackBar.open('something went wrong', 'Ok');
+      },
+    );
+  }
+
+  likePost() {
+    this.postService.likePost(this.postId).subscribe(
+      (Response) => {
+        this.matSnackBar.open('Post Liked successfully', 'Ok');
+        this.getPostById();
       },
       (error) => {
         this.matSnackBar.open('something went wrong', 'Ok');
